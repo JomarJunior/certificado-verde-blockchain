7ab07ebeb7b504cbb18033fa7ae8b6efe8146d52
JomarJunior <jomarjunior@poli.ufrj.br>
2025-11-29 21:33:56 -0300

    feat: implement __init__.py for auditors and certifiers module with application imports

---
+++ backend/src/certificado_verde_blockchain/auditors_and_certifiers/__init__.py
@@ -0,0 +1,21 @@
+from .application import (
+    FindAuditorByIdHandler,
+    FindCertifierByIdHandler,
+    ListAllAuditorsHandler,
+    ListAllCertifiersHandler,
+    RegisterAuditorCommand,
+    RegisterAuditorHandler,
+    RegisterCertifierCommand,
+    RegisterCertifierHandler,
+)
+
+__all__ = [
+    "FindCertifierByIdHandler",
+    "ListAllAuditorsHandler",
+    "ListAllCertifiersHandler",
+    "RegisterAuditorCommand",
+    "RegisterCertifierCommand",
+    "RegisterAuditorHandler",
+    "RegisterCertifierHandler",
+    "FindAuditorByIdHandler",
+]

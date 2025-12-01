0e51e3e79dfe3a334f7d47b4828699abd8fce244
JomarJunior <jomarjunior@poli.ufrj.br>
2025-12-01 00:05:16 -0300

    feat: add routes for producers, certifiers, auditors, and certificates in AppRoutes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { ProtectedRoute } from "../components/ProtectedRoute";
+import AuditorRegister from "../views/auditors/AuditorRegister";
+import AuditorsOverview from "../views/auditors/AuditorsOverview";
+import IssueCertificate from "../views/certificates/IssueCertificate";
+import PreCertificateRegister from "../views/certificates/PreCertificateRegister";
+import PreCertificatesOverview from "../views/certificates/PreCertificatesOverview";
+import VerifyCertificate from "../views/certificates/VerifyCertificate";
+import CertifierRegister from "../views/certifiers/CertifierRegister";
+import CertifiersOverview from "../views/certifiers/CertifiersOverview";
 import HomePage from "../views/HomePage";
 import NotFound from "../views/NotFound";
+import ProducersRegister from "../views/producers/ProducerRegister";
+import ProducersOverview from "../views/producers/ProducersOverview";
 import ProductRegister from "../views/products/ProductRegister";
 import ProductsOverview from "../views/products/ProductsOverview";
 import UnauthorizedPage from "../views/UnauthorizedPage";
@@ -13,7 +23,17 @@ function AppRoutes() {
             <Route index path="/" element={<HomePage />} />
             <Route path="/products" element={<ProductsOverview />} />
             <Route path="/products/register" element={<ProductRegister />} />
+            <Route path="/producers" element={<ProducersOverview />} />
+            <Route path="/producers/register" element={<ProducersRegister />} />
+            <Route path="/certifiers" element={<CertifiersOverview />} />
+            <Route path="/certifiers/register" element={<CertifierRegister />} />
+            <Route path="/auditors" element={<AuditorsOverview />} />
+            <Route path="/auditors/register" element={<AuditorRegister />} />
+            <Route path="/certificates/pre-issued" element={<PreCertificatesOverview />} />
+            <Route path="/certificates/register" element={<PreCertificateRegister />} />
+            <Route path="/certificates/verify" element={<VerifyCertificate />} />
 
+            <Route path="/certificates/issue/:id" element={<IssueCertificate />} />
 
             <Route path="/unauthorized" element={<UnauthorizedPage />} />
             <Route path="/test-protected" element={
